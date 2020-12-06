ec4e32d353410ad4adbaae81754ca36d9075fe21
HasanAli <h.alisiseci@gmail.com>
2020-12-06 12:03:30 +0300

    First commit

--- TabBarDemo/ViewController.swift
+++ TabBarDemo/ViewController.swift
@@ -8,12 +8,120 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let button: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 52))
+        button.layer.cornerRadius = 10
+        button.layer.shadowOpacity = 0.3
+        button.layer.shadowOffset = CGSize(width: 0, height: 5)
+        button.setTitle("Log In", for: .normal)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.center = view.center
+    }
+    
+    @objc func didTapButton() {
+        //Create and present tab bar controller
+        let tabBarVC = UITabBarController()
+        let vc1 = UINavigationController(rootViewController: FirstViewController())
+        let vc2 = UINavigationController(rootViewController: SecondViewController())
+        let vc3 = UINavigationController(rootViewController: ThirdViewController())
+        let vc4 = UINavigationController(rootViewController: FourthViewController())
+        let vc5 = UINavigationController(rootViewController: FifthViewController())
+        
+        vc1.title = "Home"
+        vc2.title = "Contact"
+        vc3.title = "About"
+        vc4.title = "Settings"
+        vc5.title = "Help"
 
-
+        tabBarVC.setViewControllers([vc1,vc2,vc3,vc4,vc5], animated: false)
+        
+        
+        guard let items = tabBarVC.tabBar.items else {
+            return
+        }
+        
+        let images = ["house","character.book.closed","person.circle","gear","paperclip"]
+        
+        for item in 0..<items.count {
+            //Bildirim sayısı için badgeValue
+            items[item].badgeValue = String(item+1)
+            items[item].image = UIImage(systemName: images[item])
+        }
+        
+        tabBarVC.modalPresentationStyle = .fullScreen
+        
+        present(tabBarVC,animated: true)
+    }
 }
 
+class FirstViewController: UIViewController {
+    
+    private let label : UILabel = {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
+        label.text = "Selam"
+        label.font = UIFont(name: label.font.familyName, size: 70)
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Home"
+        view.addSubview(label)
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        label.center = view.center
+    }
+}
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        title = "Contact"
+
+    }
+}
+
+class ThirdViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+        title = "About"
+
+    }
+}
+
+class FourthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+        title = "Settings"
+
+    }
+}
+
+class FifthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+        title = "Help"
+    }
+}
